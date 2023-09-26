056c8ef3512d3f07a83bec73dcaef9545591c70c
KodyAnderson1 <kody_anderson@yahoo.com>
2023-09-26 15:43:45 -0500

    SaveBookRequest record to class

--- src/main/java/com/coderiders/commonutils/models/googleBooks/SaveBookRequest.java
+++ src/main/java/com/coderiders/commonutils/models/googleBooks/SaveBookRequest.java
@@ -1,3 +1,18 @@
 package com.coderiders.commonutils.models.googleBooks;
 
-public record SaveBookRequest(String clerkId, String isbn10, String isbn13) { }
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SaveBookRequest {
+    private String clerkId;
+    private String isbn10;
+    private String isbn13;
+}
