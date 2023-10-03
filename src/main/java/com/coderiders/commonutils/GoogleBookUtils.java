ffced4c78a8c68a49241997d6922ff0a296b8561
KodyAnderson1 <kody_anderson@yahoo.com>
2023-10-03 16:57:18 -0500

    added GoogleBookUtils

---
+++ src/main/java/com/coderiders/commonutils/GoogleBookUtils.java
@@ -0,0 +1,20 @@
+package com.coderiders.commonutils;
+
+import com.coderiders.commonutils.models.googleBooks.GoogleBook;
+import com.coderiders.commonutils.models.googleBooks.IndustryIdentifier;
+
+public class GoogleBookUtils {
+    public static String getISBN10(GoogleBook googleBook) {
+        return googleBook.volumeInfo.industryIdentifiers.stream()
+                .filter(id -> id.getType().equalsIgnoreCase("ISBN_10"))
+                .findFirst().map(IndustryIdentifier::getIdentifier)
+                .orElse(null);
+    }
+
+    public static String getISBN13(GoogleBook googleBook) {
+        return googleBook.volumeInfo.industryIdentifiers.stream()
+                .filter(id -> id.getType().equalsIgnoreCase("ISBN_13"))
+                .findFirst().map(IndustryIdentifier::getIdentifier)
+                .orElse(null);
+    }
+}
