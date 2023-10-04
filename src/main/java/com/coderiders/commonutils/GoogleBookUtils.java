7c881561e4703f7b0efd45f7e728fb7e4c4e5ce6
KodyAnderson1 <kody_anderson@yahoo.com>
2023-10-04 17:23:34 -0500

    added null checks

--- src/main/java/com/coderiders/commonutils/GoogleBookUtils.java
+++ src/main/java/com/coderiders/commonutils/GoogleBookUtils.java
@@ -5,6 +5,8 @@ import com.coderiders.commonutils.models.googleBooks.IndustryIdentifier;
 
 public class GoogleBookUtils {
     public static String getISBN10(GoogleBook googleBook) {
+        if (googleBook.volumeInfo.industryIdentifiers == null) return null;
+
         return googleBook.volumeInfo.industryIdentifiers.stream()
                 .filter(id -> id.getType().equalsIgnoreCase("ISBN_10"))
                 .findFirst().map(IndustryIdentifier::getIdentifier)
@@ -12,6 +14,8 @@ public class GoogleBookUtils {
     }
 
     public static String getISBN13(GoogleBook googleBook) {
+        if (googleBook.volumeInfo.industryIdentifiers == null) return null;
+
         return googleBook.volumeInfo.industryIdentifiers.stream()
                 .filter(id -> id.getType().equalsIgnoreCase("ISBN_13"))
                 .findFirst().map(IndustryIdentifier::getIdentifier)
