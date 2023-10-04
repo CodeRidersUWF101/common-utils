61fc80d028d5189edeadec3fb278a991e49983b8
KodyAnderson1 <kody_anderson@yahoo.com>
2023-10-04 17:26:49 -0500

    Added additional items

--- src/main/java/com/coderiders/commonutils/GoogleBookUtils.java
+++ src/main/java/com/coderiders/commonutils/GoogleBookUtils.java
@@ -4,6 +4,14 @@ import com.coderiders.commonutils.models.googleBooks.GoogleBook;
 import com.coderiders.commonutils.models.googleBooks.IndustryIdentifier;
 
 public class GoogleBookUtils {
+
+    /**
+     * Retrieves the ISBN-10 identifier from a GoogleBook object.
+     *
+     * @param googleBook the GoogleBook object from which to extract the ISBN-10 identifier.
+     * @return the ISBN-10 identifier as a String, or null if the identifier is not present or if
+     * the industryIdentifiers field is null.
+     */
     public static String getISBN10(GoogleBook googleBook) {
         if (googleBook.volumeInfo.industryIdentifiers == null) return null;
 
@@ -13,6 +21,13 @@ public class GoogleBookUtils {
                 .orElse(null);
     }
 
+    /**
+     * Retrieves the ISBN-13 identifier from a GoogleBook object.
+     *
+     * @param googleBook the GoogleBook object from which to extract the ISBN-13 identifier.
+     * @return the ISBN-13 identifier as a String, or null if the identifier is not present or if
+     * the industryIdentifiers field is null.
+     */
     public static String getISBN13(GoogleBook googleBook) {
         if (googleBook.volumeInfo.industryIdentifiers == null) return null;
 
@@ -21,4 +36,54 @@ public class GoogleBookUtils {
                 .findFirst().map(IndustryIdentifier::getIdentifier)
                 .orElse(null);
     }
+
+    /**
+     * Converts the authors list of a GoogleBook object to a string array.
+     *
+     * @param googleBook the GoogleBook object from which to extract the authors.
+     * @return a string array containing the authors, or an empty array if the authors list is null.
+     */
+    public static String[] getAuthors(GoogleBook googleBook) {
+        return googleBook.volumeInfo.authors == null
+                ? new String[0]
+                : googleBook.volumeInfo.authors.toArray(new String[0]);
+    }
+
+    /**
+     * Converts the categories list of a GoogleBook object to a string array.
+     *
+     * @param googleBook the GoogleBook object from which to extract the categories.
+     * @return a string array containing the categories, or an empty array if the categories list is null.
+     */
+    public static String[] getCategories(GoogleBook googleBook) {
+        return googleBook.volumeInfo.categories == null
+                ? new String[0]
+                : googleBook.volumeInfo.categories.toArray(new String[0]);
+    }
+
+    /**
+     * Retrieves the small thumbnail link of a GoogleBook object.
+     *
+     * @param googleBook the GoogleBook object from which to extract the small thumbnail link.
+     * @return the small thumbnail link as a string, or null if the imageLinks or smallThumbnail field is null.
+     */
+    public static String getSmallThumbnail(GoogleBook googleBook) {
+        return googleBook.volumeInfo.imageLinks != null && googleBook.volumeInfo.imageLinks.smallThumbnail != null
+                ? googleBook.volumeInfo.imageLinks.smallThumbnail
+                : null;
+    }
+
+    /**
+     * Retrieves the thumbnail link of a GoogleBook object.
+     *
+     * @param googleBook the GoogleBook object from which to extract the thumbnail link.
+     * @return the thumbnail link as a string, or null if the imageLinks or thumbnail field is null.
+     */
+    public static String getThumbnail(GoogleBook googleBook) {
+        return googleBook.volumeInfo.imageLinks != null && googleBook.volumeInfo.imageLinks.thumbnail != null
+                ? googleBook.volumeInfo.imageLinks.thumbnail
+                : null;
+    }
+
+
 }
