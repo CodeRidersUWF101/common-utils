cc063b1d15f062f7769688148048e9dabc2385f9
KodyAnderson1 <kody_anderson@yahoo.com>
2023-10-18 17:28:06 -0500

    Updated

--- src/main/java/com/coderiders/commonutils/models/UserChallengesExtraDTO.java
+++ src/main/java/com/coderiders/commonutils/models/UserChallengesExtraDTO.java
@@ -20,8 +20,8 @@ public class UserChallengesExtraDTO {
     private long userChallengeId;
     private String name;
     private String description;
-    private ChallengeFrequency frequency;
-    private BadgeType type;
+    private String frequency;
+    private String type;
     private int threshold;
     private int duration;
     private LocalDateTime challengeStartDate;
@@ -34,41 +34,5 @@ public class UserChallengesExtraDTO {
 
     private DateProgress dateProgress;
     private AdditionalChallengeInfo additionalInfo;
-
-
-    public static UserChallengesExtraDTO userChallengesToDTO(UserChallengesDTO dto) {
-        return UserChallengesExtraDTO.builder()
-                .id(dto.id())
-                .userChallengeId(dto.userChallengeId())
-                .name(dto.name())
-                .description(dto.description())
-                .frequency(dto.frequency())
-                .type(dto.type())
-                .threshold(dto.threshold())
-                .duration(dto.duration())
-                .challengeEndDate(dto.challengeEndDate())
-                .challengeStartDate(dto.challengeStartDate())
-                .pointsAwarded(dto.pointsAwarded())
-                .userChallengeStartDate(dto.userChallengeStartDate())
-                .status(dto.status())
-                .build();
-    }
-
-    public static UserChallengesExtraDTO readingChallengeToDTO(ReadingChallenges challenges) {
-        return UserChallengesExtraDTO.builder()
-                .id(challenges.getId())
-                .name(challenges.getName())
-                .description(challenges.getDescription())
-                .frequency(challenges.getFrequency())
-                .type(challenges.getType())
-                .duration(challenges.getDuration())
-                .threshold(challenges.getThreshold())
-                .challengeEndDate(challenges.getEndDate())
-                .challengeStartDate(challenges.getStartDate())
-                .pointsAwarded(challenges.getPointsAwarded())
-                .build();
-
-    }
-
 }
 
