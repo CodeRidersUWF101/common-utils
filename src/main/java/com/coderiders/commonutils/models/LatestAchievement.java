7d906ca6efc0507f81c925896bf0584cb485d32e
KodyAnderson1 <kody_anderson@yahoo.com>
2023-10-21 14:35:19 -0500

    Updated

---
+++ src/main/java/com/coderiders/commonutils/models/LatestAchievement.java
@@ -0,0 +1,13 @@
+package com.coderiders.commonutils.models;
+
+import java.time.LocalDateTime;
+
+
+public class LatestAchievement {
+    private String name;
+    private String type;
+    private String description;
+    private int threshold;
+    private LocalDateTime dateEarned;
+
+}
