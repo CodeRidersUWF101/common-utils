ecba0be16f18c5c6c8d350b05df7b36f003093ee
KodyAnderson1 <kody_anderson@yahoo.com>
2023-10-26 17:17:21 -0500

    Updated

--- src/main/java/com/coderiders/commonutils/models/BookStats.java
+++ src/main/java/com/coderiders/commonutils/models/BookStats.java
@@ -1,6 +1,5 @@
 package com.coderiders.commonutils.models;
 
-import com.coderiders.commonutils.models.enums.ActivityAction;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,15 +7,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
 
-import java.util.List;
-
 @Data
 @Builder
 @Jacksonized
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class SingleBookStats {
-    private List<BookStats> bookStats;
-    private Status status;
+public class BookStats {
 }
